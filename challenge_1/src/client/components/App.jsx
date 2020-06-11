52ff562e0197f4094a116a996bd66466e86a10b1
Joey Nguyen <joeylnguyen95@gmail.com>
2020-06-10 18:04:58 -0700

    Get event data from query

--- challenge_1/src/client/components/App.jsx
+++ challenge_1/src/client/components/App.jsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 const App = () => {
+  const [ eventData, setEventData ] = useState([]);
+  const [ query, setQuery ] = useState('');
+
+  const handleChange = (event) => {
+    setQuery(event.target.value);
+  };
+  const handleSubmit = () => {
+    event.preventDefault();
+    getEventData(query)
+  };
+
+  const getEventData = (query) => {
+    console.log(query);
+    axios.get(`http://localhost:3000/events?q=${query}`)
+      .then((results) => console.log(results))
+      .catch((error) => console.log(error));
+  };
+
   return (
     <div>
-      Sup bitch, why is live server lagg???
+      <form onSubmit={handleSubmit}>
+        <label>
+          Search Historical Events:
+          <input type="text" onChange={handleChange}/>
+        </label>
+        <input type="submit" value="Search"/>
+      </form>
     </div>
   )
 };
